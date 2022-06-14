c3bb970ff4e9b320439ab962db9997bb689169b3
luis-ao <96146916+luis-ao@users.noreply.github.com>
2022-06-14 18:41:10 -0500

    env

--- src/utils/httpClients.js
+++ src/utils/httpClients.js
@@ -1,10 +1,11 @@
-const API = "https://api.themoviedb.org/3";
+const API = process.env.REACT_APP_API;
+const API_TOKEN = process.env.REACT_APP_API_TOKEN;
 
 export function get(path) {
   return fetch(API + path, {
     headers: {
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNzA1Mjc2ZjM4M2EyMDg4OTk2MDFmYmMzN2Y4YjU4NiIsInN1YiI6IjYyNDM0OGY4Yzc0MGQ5MDA0N2FkNDc1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FmNNiDK7VWIieEZHx_gRzGarMU0slipFw9A0tmZZ2hw",
+        "Bearer " + API_TOKEN,
       // Al ser una separada por un guión es obligatorio usar doble comilla
       "Content-Type": "application/json;charset=utf-8",
     },
